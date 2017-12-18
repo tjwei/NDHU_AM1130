4e5b76a01859f9cdb65e53419b0ddd8770b96be5
tjwei <tzerjen@gmail.com>
2017-12-18 13:53:45 +0800

    Update week14_flappy_something.py

--- week14_flappy_something.py
+++ week14_flappy_something.py
@@ -4,12 +4,25 @@ import math
 pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
-bird = pygame.image.load(r"C:\Users\tjw\Pictures\frame-1.png")
-bird = pygame.transform.scale(bird, (50,42))
-for i in range(300):
+birds = []
+for i in range(1, 9):
+    #bird = pygame.image.load(r"C:\Users\tjw\Pictures\frame-%d.png"%i)
+    bird = pygame.image.load(r"C:\Users\tjw\Pictures\frame-{}.png".format(i))
+    bird = pygame.transform.scale(bird, (50,42))
+    birds.append(bird)
+    
+bird_x = width//2-140
+bird_y = height//2
+running = True
+i = 0
+while running:       
     screen.fill((0,0,0))
-    screen.blit(bird, (width//2, height//2 + height//3 * math.sin(i*2*math.pi/30)))
+    screen.blit(birds[abs( (i//3)%15-7)], (bird_x, bird_y ))
     pygame.display.flip()
     sleep(1./30)
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            running = False
+    i+=1
 pygame.quit()
